785850ff4eb0b70179c60c7b71d890757f604ba6
Laurenjachimczyk <63621274+Laurenjachimczyk@users.noreply.github.com>
2024-03-23 15:24:27 -0400

    Update fetch_data.sh
    update to csv format instead of json

--- fetch_data.sh
+++ fetch_data.sh
@@ -5,25 +5,25 @@ limit=1000
 output_file="parking_violations.csv"
 iteration=0
 
-# Define the list of columns to fetch
+#limit to only the columns needed
 columns="summons_number,plate_id,registration_state,issue_date,vehicle_make,vehicle_year,street_name,vehicle_color"
 
-# Get first 1000 rows and write to file
+#get first 1000 rows and write to file
 curl "https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?\$select=$columns&\$limit=$limit&\$offset=$offset" > "$output_file"
 
-# Get remaining data in increments of 1000 rows and append to file
+#get remaining data in increments of 1000 rows and append to file
 while true; do
     iteration=$((iteration + 1))
     echo "Iteration: $iteration"
 
-    # Print API url to confirm offset is updating 
+    #print API url to confirm offset is updating 
     api_url="https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?\$select=$columns&\$limit=$limit&\$offset=$((offset + limit))"
     echo "API request URL: $api_url"
 
-    # Get and append to file
+    #get data and append to file
     curl "$api_url" >> "$output_file"
 
-    # Check if all data is there
+    #check if all data is there
     if [ $(wc -l < "$output_file") -lt $((offset + limit)) ]; then
         echo "All data fetched."
         break
