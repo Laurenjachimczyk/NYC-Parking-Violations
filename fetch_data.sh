3ce497f26b7e46c097834b63468c69e31dc05c8e
Laurenjachimczyk <63621274+Laurenjachimczyk@users.noreply.github.com>
2024-03-23 15:22:54 -0400

    Update fetch_data.sh
    update to csv format instead of json

--- fetch_data.sh
+++ fetch_data.sh
@@ -2,26 +2,29 @@ echo "Starting.."
 
 offset=0
 limit=1000
-output_file="parking_violations.json"
+output_file="parking_violations.csv"
 iteration=0
 
-#get first 1000 rows and write to file
-curl "https://data.cityofnewyork.us/resource/pvqr-7yc4.json?\$limit=$limit&\$offset=$offset" | jq '.' > "$output_file"
+# Define the list of columns to fetch
+columns="summons_number,plate_id,registration_state,issue_date,vehicle_make,vehicle_year,street_name,vehicle_color"
 
-#get remaining data in increments of 1000 rows and append to file
+# Get first 1000 rows and write to file
+curl "https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?\$select=$columns&\$limit=$limit&\$offset=$offset" > "$output_file"
+
+# Get remaining data in increments of 1000 rows and append to file
 while true; do
     iteration=$((iteration + 1))
     echo "Iteration: $iteration"
 
-    #print API url to confirm offset is updating 
-    api_url="https://data.cityofnewyork.us/resource/pvqr-7yc4.json?\$limit=$limit&\$offset=$((offset + limit))"
+    # Print API url to confirm offset is updating 
+    api_url="https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?\$select=$columns&\$limit=$limit&\$offset=$((offset + limit))"
     echo "API request URL: $api_url"
 
-    #get and append to file
-    curl "$api_url" | jq '.' >> "$output_file"
+    # Get and append to file
+    curl "$api_url" >> "$output_file"
 
-    #check if all data is there
-    if [ $(grep -c '{' "$output_file") -lt $((offset + limit)) ]; then
+    # Check if all data is there
+    if [ $(wc -l < "$output_file") -lt $((offset + limit)) ]; then
         echo "All data fetched."
         break
     fi
